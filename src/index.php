bd3881b7ae520bd0be22eef63c71120d807335ed
GuiJ14 <88312482+GuiJ14@users.noreply.github.com>
2021-10-05 14:35:47 +0200

    first draft

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Grinto\Index;
+
+$sql = file_get_contents('sql');
+if(isset($sql)){
+	try {
+    $dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+    $dbh->query($sql);
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+}
